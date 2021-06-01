fec70b5cdedbcfe83e19e74840bea54644289079
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-06-01 19:22:11 -0400

    code added

--- Algorithms03.cpp
+++ Algorithms03.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int ar3() {
 
+	/*
+	char c;
+	cout << "Enter a lowercase letter: ";
+	cin >> c;
+
+	// covert c into integer
+	// int i = c; implicit
+	int i = static_cast<int>(c);
+
+	// substract 32 from integer equivalent
+	i -= 32;
+	// convert modified integer back into character and store into c
+	c = i; // implicit
+	// c = static_cast<char>(i); // explicit
+
+	/*
+	* This is a single line solution
+	* c = static_cast<char>(static_cast<int>(c)-('a'-'A'));
+	*/
+
+	// cout << "The uppercase equivalent is " << c << endl;
+
 	float myNumber;
 	cout << "Enter a floating-point value: ";
 	cin >> myNumber;
--- VariablesPractice.cpp
+++ VariablesPractice.cpp
@@ -1,31 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-	/*
-	char c;
-	cout << "Enter a lowercase letter: ";
-	cin >> c;
+int vp() {
 	
-	// covert c into integer
-	// int i = c; implicit
-	int i = static_cast<int>(c);
-
-	// substract 32 from integer equivalent
-	i -= 32;
-	// convert modified integer back into character and store into c
-	c = i; // implicit
-	// c = static_cast<char>(i); // explicit
-
-	/*
-	* This is a single line solution 
-	* c = static_cast<char>(static_cast<int>(c)-('a'-'A'));	
-	*/
-
-	// cout << "The uppercase equivalent is " << c << endl;
-	
-
-	/*
 	// 1.
 	string name;
 	string middleI;
@@ -54,8 +31,32 @@ int main() {
 		<< age << ", "
 		<< course << ", "
 		<< gpa << ", "
+		<< endl
 		<< endl;
-	*/
+	
+
+	// 2.
+	double length;
+	double width;
+	cout << "What is the length of the rectangle? ";
+	cin >> length;
+	cout << endl;
+	cout << "What is the width of the rectangle: ";
+	cin >> width;
+	cout << endl << endl;
+	cout << "The rectangle's area is " << (length * width) << endl << endl;
+	
+
+	// 3. 
+	int x = (3 * 2) * (2 * 2) - 10;
+	cout << x << endl << endl;
+	
+
+	// 4.
+	double d = 5.3;
+	bool b = 0;
+	int x = b = (5 + 1) + d;
+	cout << x << endl;
 
 	return 0;
 }
