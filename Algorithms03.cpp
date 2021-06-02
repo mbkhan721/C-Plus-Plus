c1ddb292633e3ab4f1963d02e1c4184e10c9dc65
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-06-01 20:11:06 -0400

    code added

--- Algorithms03.cpp
+++ Algorithms03.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int ar3() {
+int main() {
 
 	/*
 	char c;
@@ -24,7 +24,7 @@ int ar3() {
 	*/
 
 	// cout << "The uppercase equivalent is " << c << endl;
-
+	/*
 	float myNumber;
 	cout << "Enter a floating-point value: ";
 	cin >> myNumber;
@@ -33,6 +33,44 @@ int ar3() {
 	int i = static_cast<int>(myNumber);
 	cout << "The integer value is: " << i << endl;
 	cout << "The remaining decimal value is: " << (myNumber - i) << endl;
+	*/
+
+	/*
+	// 1. Implicit version ---------------- lower to upper
+	char c;
+	cout << "Enter a lowercase letter: ";
+	cin >> c;
+	// convert the char into integer
+	int i = c;
+	// subsstract 32 from integer
+	i -= 32;
+	// convert modified int back to char
+	c = i;
+	cout << endl; 
+	cout << "The uppercase equivalent is: " << c << endl;
+	*/
+
+
+	/*
+	// 1b Explicit version ----------------- upper to lower
+	char z;
+	cout << "Enter a uppercase letter: ";
+	cin >> z;
+	cout << endl;
+	int x = static_cast<int>(z); // converting char into int
+	x = x + 32;
+	z = static_cast<char>(x); // converting int back to char
+	cout << "The lowercase version is: " << z << endl;
+	*/
+
+	
+	// lower to upper ------------------ single line solution 
+	char y;
+	cout << "Enter a lowercase letter: ";
+	cin >> y;
+	cout << endl;
+	y = static_cast<char>(static_cast<int>(y) - ('b' - 'B'));
+	cout << "The uppercase equivalent is: " << y << endl;
 
 	return 0;
 }
--- VariablesPractice.cpp
+++ VariablesPractice.cpp
@@ -55,7 +55,7 @@ int vp() {
 	// 4.
 	double d = 5.3;
 	bool b = 0;
-	int x = b = (5 + 1) + d;
+	//int x = b = (5 + 1) + d;
 	cout << x << endl;
 
 	return 0;
