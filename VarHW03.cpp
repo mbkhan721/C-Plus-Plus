63691c3faa3a06022518354363ca057c42de611a
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-06-02 22:49:35 -0400

    Code added

---
+++ VarHW03.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	// 1.
+	char v;
+	cout << "Input an uppercase character: ";
+	cin >> v;
+	v = static_cast<int>(v); // converting char to int
+	v += 32;
+	v = static_cast<char>(v); // converting int back to char
+	cout << "The lowercase equivalent is '" << v << "'." << endl;
+
+	// 2.
+	char w;
+	cout << "Input an uppercase character: ";
+	cin >> w;
+	w = static_cast<char>(static_cast<int>(w) + (32));
+	cout << "The lowercase equivalent is '" << w << "'." << endl;
+	
+	// 3.
+	float a;
+	float b;
+	cout << "Enter a floating-point value: ";
+	cin >> a;
+	cout << "Enter a floating-point value: ";
+	cin >> b;
+	int c;
+	c = static_cast<int>(a) + static_cast<int>(b);
+	cout << "Sum of the integers: " << c << endl;
+
+	return 0;
+}
