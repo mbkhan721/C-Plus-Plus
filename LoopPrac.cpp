6f90b0b50d89e9033c27cae3c3a97581f4344ad3
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-06-09 02:20:25 -0400

    Loops practice problems added

--- LoopPrac.cpp
+++ LoopPrac.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 
-int loopPrac() {
-
+int main() {
+	cout << endl;
+	/*
+	cout << endl;
 	int x = 10;
 	
 	while (x >= 10) {
@@ -23,7 +25,7 @@ int loopPrac() {
 		--j;
 	}
 	cout << endl << endl;
-	/*
+	
 	// 10.
 	int min, max;
 	cout << "Enter a min and max value: ";
@@ -53,7 +55,7 @@ int loopPrac() {
 			cout << i << " ";
 		}
 	} 
-	*/
+	
 	cout << endl << endl;
 
 	// 2.
@@ -68,10 +70,85 @@ int loopPrac() {
 		count += 1;
 	}
 	cout << count << " digits.";
+	
+	// while loop
+	int n;
+	cout << "Enter a number between 1 and 10 inclusive: ";
+	cin >> n;
+	while (n < 1 || n > 10) {
+		cout << "Invalid value, enter a num bet 1 & 10 inclusive: ";
+		cin >> n;
+	}
+	cout << "\nProgram continues if value is valid or invalid.\n";
+	
+
+	// Do While loop *************************************************
+	int n;
+	do {
+		cout << "Enter a number between 1 and 10 inclusive: ";
+		cin >> n;
+	} while
+		(n < 1 || n > 10);
+	cout << "\nProgram continues if value is valid or invalid.\n";
+	
+
+	// For loop *************************************************
+	for (int i = 1; i < 11; ++i) {
+		cout << i << " ";
+	}
+	cout << "\n\n";
+	--------------------------------------------------- */
+	/* 
+	for (initialize var, condition, update var) {
+		code to repeat
+	}
+
+	variable updated to eventually make the condition false
+	variable initialized with value a
+	condition specifies the limit b
+	variable updated every iteration to eventually reach b
+	*/
+	/*
+	for (int i = 20; i >= 10; --i) {
+		cout << i << " ";
+	}
+	
+
+	// while loop
+	cout << "\n\nWhile Loop:\n" << endl;
+	int i = 1;
+	while (i < 11) {
+		cout << i << " ";
+		++i;
+	}
+	*/
+
+	int n;
+	cout << "Enter an integer: ";
+	cin >> n;
+
+	int summation = 0, factorial = 1;
+	// summation is adding every value from a to b
+
+	for (int i = 1; i <= n; ++i) {
+		summation += i;
+	}
+	for (int i = 1; i <= n; ++i) {
+		factorial *= i;
+	}
+
+	cout << summation << "\n";
+	cout << factorial << "\n";
+	
 
 
 
 
 
+
+
+
+
+	cout << endl;
 	return 0;
 }
