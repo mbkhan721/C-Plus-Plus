5f77a51ac97867836128b2247510c1f47e7e1c5a
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-06-11 19:54:44 -0400

    Nested Loops code added

---
+++ NestedLoopsEx.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << endl;
+
+	// CONCEPT
+	// this nested loop processes a total of i * j iterations,
+	// or 2 * 3 == 6 iterations in total
+
+	cout << "i	j" << endl;
+	cout << "----------" << endl;
+	for (int i = 1; i <= 2; ++i) {
+		for (int j = 1; j <= 4; ++j) {
+			cout << i << "	" << j << "\n";
+		}
+		cout << "\n";
+	}
+
+
+	// IMAGES 
+	// a two-dimensional image of rows and columns
+	
+	cout << "Example 2:\n\n";
+	for (int r = 1; r <= 3; ++r) {
+		for (int c = 1; c <= 4; ++c) {
+			cout << "* ";			
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+
+
+	cout << "Coordinates (i, j)\n";
+	for (int r = 1; r <= 3; ++r) {
+		for (int c = 1; c <= 4; ++c) {
+			cout << r << "," << c << " ";
+		}
+		cout << "\n";
+	}
+
+
+	cout << "\nExample 3:\n";
+	cout << "Unique row\n";
+	for (int r = 1; r <= 7; ++r) {
+		for (int c = 1; c <= 7; ++c) {
+			if (r == 5) {
+				cout << "= ";
+			}
+			else {
+				cout << "* ";
+			}
+		}
+		cout << "\n";
+	}
+
+
+	cout << "\nExample 4:\n";
+	cout << "Alternating Columns\n";
+	for (int r = 1; r <= 7; ++r) {
+		for (int c = 1; c <= 7; ++c) {
+			if (c % 2 == 0) {
+				cout << "* ";
+			}
+			else {
+				cout << "x ";
+			}
+		}
+		cout << "\n";
+	}
+
+	cout << "\nExample 5:\n";
+	cout << "Alternating Columns and Rows\n";
+	for (int r = 1; r <= 7; ++r) {
+		for (int c = 1; c <= 7; ++c) {
+			if (c % 2 == 0 && r % 2 == 0) {
+				cout << "* ";
+			}
+			else {
+				cout << "x ";
+			}
+		}
+		cout << "\n";
+	}
+
+	cout << "\nExample 6:\n";
+	cout << "Alternating Columns and Rows\n";
+	for (int r = 1; r <= 7; ++r) {
+		for (int c = 1; c <= 7; ++c) {
+			if (c % 2 == 0 && r % 2 == 0) {
+				cout << r << "x" << c << " ";
+			}
+			else {
+				cout << "... ";
+			}
+		}
+		cout << "\n";
+	}
+
+	cout << "\nExample 7:\n";
+	cout << "Triangle\n";
+	for (int r = 1; r <= 10; ++r) {
+		for (int c = r; c <= 10; ++c) {
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\nExample 7:\n";
+	cout << "Triangle II\n";
+	for (int r = 1; r <= 10; ++r) {
+		for (int c = 1; c <= r; ++c) {
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\nExample 8:\nPrint all factors of each number from 1 to n inclusive\n";
+	for (int n = 1; n <= 6; ++n) {
+		cout << "Factors of " << n << ": ";
+		for (int f = 1; f <= n; ++f) {
+			if (n % f == 0) {
+				cout << f << " ";
+			}
+		}
+		cout << "\n";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	cout << endl;
+	return 0;
+}
