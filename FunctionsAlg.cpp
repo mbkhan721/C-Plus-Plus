4f1d6c97f127378e62699e89529cdf6f68e66b5c
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-06-14 22:33:52 -0400

    Code added

--- FunctionsAlg.cpp
+++ FunctionsAlg.cpp
@@ -1,16 +1,58 @@
 #include<iostream>
 #include<string>
 using namespace std;
+/*
+int getFirstIndex(string s, char c);
 
-//int getFirstIndex(string s, char c);
-
-// string reverse(string s);
+string reverse(string s);
 
 void validate(string s);
 
+int factorial(int n);
+
+bool isFactor(int a, int b) {
+	if (b % a == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+char getLastIndex(string str, char s) {
+	for (int i = str.length() - 1; i >= 0; --i) {
+		if (str[i] == 's') {
+			return i;
+		}
+		return -1;
+	}
+}
+
+int getNumDigits(int n) {
+	int count = 0;
+	while (n != 0) {
+		n /= 10;
+		++count;
+	}
+	return count;
+}
+
+double getMax(int a, double b) {
+	if (static_cast<double>(a) > b) {
+		return a;
+	}
+	return b;
+}
+
+int getAsciiValue(char a) {
+	//a = static_cast<int>(a) + ('B' - 'b');
+	return (int) a;
+}
+
+
 int main() {
 	cout << endl;
-	/*
+	
 	// 1.
 	cout << "1.\n";
 	string word;
@@ -25,21 +67,102 @@ int main() {
 	cout << "Enter a string: ";
 	getline(cin, str);
 	cout << reverse(str) << endl;
-	*/
+	
 	// 3.
 	cout << "\n3.\n";
 	validate("2!a!a!");
-
+	
 	// 4.
 	cout << "\n4.\n";
+	int n;
+	cout << "Enter a value for n: ";
+	cin >> n;
+	cout << "n! = " << factorial(n) << endl;
+	
+	// 5.
+	const int DIM = 5;
+	for (int row = 1; row <= DIM; ++row) {
+		for (int col = 1; col <= DIM; ++col) {
+			if (row % 2 != 0 && col % 2 != 0) {
+				cout << "x ";
+			}
+			else if (col == DIM / 2 + 1 ) {
+				cout << "x ";
+			}
+			else
+				cout << "o ";
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
 
+	const int D = 5; // must work for D==3 and D==5
+
+	for (int r = 1; r <= D; ++r) {
+		for (int c = 1; c <= D; ++c) {
+			if (c > r || c == r - 2) { // line 2 SW of \ OR all coordinates NW of 
+				cout << "X ";
+			}
+			else {
+				cout << "0 ";
+			}
+		}
+		cout << "\n";
+	}
+
+	cout << endl << endl;
+	
+	int sum;
+	int max = 10;
+	for (int i = 0; i <= max; i++) {
+		sum = 0;
+		for (int j = 1; j <= i; j++) {
+			sum += j;
+		}
+		cout << sum << endl;
+	}
+
+	string s;
+	cout << "TEST Enter a string of any length: ";
+	cin >> s;
+	for (int i = 0; i < s.length(); ++i) {
+		if (s[i%2!=0]) {
+			cout << s[i];
+		}
+	}
+	cout << endl << endl;
+
+	// cout << getMax(10, 15.5) << endl;
+
+	// cout << getAsciiValue('A') << endl;
+	cout << "\n3.\n";
+	int o = 5;
+
+	for (int r = 1; r <= o; ++r) {
+		for (int c = 1; c <= o; ++c) {
+			if (c <= r) {
+				cout << "x";
+			}
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
+
+	cout << endl << endl;
+	int x;
+	do {
+		cout << "Enter an int: ";
+		cin >> x;
+	} while (x % 10 != 0);
+	
 
 
 
 	cout << endl;
 	return 0;
 }
-/*
+
 int getFirstIndex(string s, char c) {
 	for (int i = 0; i < s.length(); i++) {
 		if (c == s[i]) {
@@ -58,7 +181,7 @@ string reverse(string s) {
 	}
 	return s;
 }
-*/
+
 void validate(string s) {
 	if (s[0] >= 'a' && s[0] <= 'z') {	// if first char is letter, invalid, end function early
 		cout << s << " Invalid" << endl;
@@ -73,3 +196,13 @@ void validate(string s) {
 	}
 	cout << s << " Valid" << endl;
 }
+
+int factorial(int n) {
+	int product = 1;									// intialize product to 1
+	for (int i = n; i > 1; --i) {						// count from n down to 2
+		product *= i;									// multiply product by i each time 
+	}
+	return product;										// return product
+}
+
+*/
