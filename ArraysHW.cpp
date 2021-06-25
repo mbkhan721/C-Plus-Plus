42ba59fa28726969ab78a856a37c12136c327ca6
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-06-24 22:46:13 -0400

    Arrays homework added

--- ArraysHW.cpp
+++ ArraysHW.cpp
@@ -5,30 +5,36 @@ using namespace std;
 
 int main() {
 
+	cout << "\n1. " << "\n";
+	int a[5];								// declare an array of 5 integers
 
-	srand(time(NULL));
-	const int SIZE = 10;
-	int b[SIZE];
-
-	for (int i = 0; i < SIZE; i++) {
-		b[i] = rand() % 21;
+	for (int i = 0; i < 5; i++) {
+		cout << "[" << i << "] = ";
+		cin >> a[i];						// input values from the console into the array
+	}
+	cout << "\nArray: ";
+	for (int i = 0; i < 5; i++) {
+		cout << a[i] << " ";				// loop to output the array
 	}
 
-	for (int i = 0; i < SIZE; i++) {
-		cout << b[i] << " ";
-	}
-
-	int maxDif = 0;
-	for (int i = 1; i < SIZE; i++) {
-		int diff = b[i] - b[i - 1];					// current - previous
-		if (diff < 0) {
-			diff = -diff;							// If difference is a negative number, negate it
-		}											// Make it a positive number to get abs value of curr-prev
-		if (diff > maxDif) {
-			return maxDif;							// everytime I find the largest value, I update the maxDiff to that value
+	int min = a[0];
+	for (int i = 1; i < 5; i++) {			// iterate from the second to last value of the array
+		if (min > a[i]) {					// if the current min is larger then the current value
+			min = a[i];						// replace min with the current value
 		}
 	}
-	cout << "\nMax diff = " << maxDif << endl << endl;
+	cout << "\nMin: " << min << endl;		// determine the smallest element in the array.
+
+	int max = a[0];
+	for (int i = 1; i < 5; i++) {			// iterate from the second to last value of the array
+		if (max < a[i]) {					// if the current max is smaller then the current value
+			max = a[i];						// replace max with the current value
+		}
+	}
+	cout << "\Max: " << max << endl;		// determine the largest element in the array.
+
+
+
 
 
 
